1e9bc98a66c4db46414e726205ccdb6c998e8ded
Sivanathan B <sivanathan2903@gmail.om>
2023-11-27 22:54:58 +0000

    test spanner

--- routes/root.js
+++ routes/root.js
@@ -1,18 +1,45 @@
 'use strict'
 
+const { Spanner } = require('@google-cloud/spanner');
+
 module.exports = async function (fastify, opts) {
-    fastify.post('/token', (req, reply, payload) => {
-        const token = fastify.jwt.sign({ payload });
+    fastify.post('/token', async (req, reply, payload) => {
+        //const token = fastify.jwt.sign({ payload });
+        const projectId = 'test-deploy-nodejs-gcp-project';
+        const instanceId = 'test-instance';
+        const databaseId = 'test-database-1';
+
+        const spanner = new Spanner({ projectId: projectId });
+
+        // Gets a reference to a Cloud Spanner instance and database
+        const instance = spanner.instance(instanceId);
+        const database = instance.database(databaseId);
+
+        // The query to execute
+        const query = {
+            sql: 'SELECT *from Personal',
+        };
+
+        // Execute a simple SQL statement
+        const [rows] = await database.run(query);
+        console.log(`Query: ${rows.length} found.`);
+        let json = {};
+        rows.forEach(row => {
+            console.log(row);
+            json = row.toJSON();
+        });
+
+
+
+
         reply
             .code(200)
-            .send(token);
+            .send(json);
     });
 
     fastify.get('/verify', {
         onRequest: [fastify.authenticate]
-    },
-        async function (request, reply) {
-            return request.user;
-        }
-    )
+    }, async function (request, reply) {
+        return request.user;
+    });
 }
